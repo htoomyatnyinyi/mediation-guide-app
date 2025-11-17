17baa86710de4140c07d8f7853579be7f14e1bd1
htoomyatnyinyi <htoomyatnyinyi@gmail.com>
2025-11-17 20:12:16 +0700

    revise nextjs config and css

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // Explicitly handle MP3 for Turbopack/Webpack fallback
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.module.rules.push({
-        test: /\.(mp3|wav)$/,
-        type: "asset/resource",
-        generator: {
-          filename: "static/media/[name].[hash][ext]",
-        },
-      });
-    }
-    return config;
-  },
+  // // Explicitly handle MP3 for Turbopack/Webpack fallback
+  // webpack: (config, { isServer }) => {
+  //   if (!isServer) {
+  //     config.module.rules.push({
+  //       test: /\.(mp3|wav)$/,
+  //       type: "asset/resource",
+  //       generator: {
+  //         filename: "static/media/[name].[hash][ext]",
+  //       },
+  //     });
+  //   }
+  //   return config;
+  // },
   experimental: {
     // Turbopack is not available as a configuration option in this version
   },
